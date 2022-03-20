8149416b49846cbc3e4b0107ee458bdec1720665
Daniel Paz <danielnicolaspeace@gmail.com>
2022-03-20 12:22:48 -0300

    final

--- tp1_2.c
+++ tp1_2.c
@@ -1,11 +1,18 @@
+#include <stdio.h>
+
 int cuadrado(int num);
 void cuadradoV(int *num);
-void mostrar(int var);
+void mostrar(int *var);
 void invertir(int *a, int *b);
 void orden(int *a, int *b);
 
 int main(int argc, char const *argv[])
-{
+{   
+    int var1;
+    int var2;
+    scanf("%d %d", &var1, &var2);
+    mostrar(&var1);
+    mostrar(&var2);
     
     return 0;
 }
@@ -18,9 +25,9 @@ void cuadradoV(int *num){
     *num = cuadrado(*num);
 }
 
-void mostrar(int var){
-    printf("Direccion de la variable: %p\n", &var);
-    printf("Contenido de la variable: %d\n", var);
+void mostrar(int *var){
+    printf("Direccion de la variable: %p\n", var);
+    printf("Contenido de la variable: %d\n", *var);
 }
 
 void invertir(int *a, int *b){
